81a6bee33bf7793c999be161d4b45ba95882bd9c
doubttttt <vmichaelmartinez@gmail.com>
2024-12-13 01:18:04 -0700

    remove border

--- components/logo-square.tsx
+++ components/logo-square.tsx
@@ -5,10 +5,10 @@ export default function LogoSquare({ size }: { size?: 'sm' | undefined }) {
   return (
     <div
       className={clsx(
-        'flex flex-none items-center justify-center border border-neutral-200 bg-white dark:border-neutral-700 dark:bg-black',
+        'flex flex-none items-center justify-center',
         {
-          'h-[40px] w-[40px] rounded-xl': !size,
-          'h-[30px] w-[30px] rounded-lg': size === 'sm'
+          'h-[40px] w-[40px]': !size,
+          'h-[30px] w-[30px]': size === 'sm'
         }
       )}
     >
